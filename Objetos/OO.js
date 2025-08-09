02b4ab48d291ba367b327a496852982bda26f391
João Victor <jvictor.sg@outlook.com>
2025-08-08 21:43:20 -0300

    Estudos de OO com JS

---
+++ Objetos/OO.js
@@ -0,0 +1,17 @@
+//CÓDIGO NÃO EXECUTAVEL!!
+
+//Programação procedural
+ProcessingInstruction(val1, val2, val3)
+//.....
+
+//OO
+objeto = {
+    val1,
+    val2,
+    val3,
+    processamento(){
+        // ...
+    }
+}
+
+objeto.processamento()
